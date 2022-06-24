6ea170c9648ae2161a2cfafded6f8ae8baedb0d4
Carlos Georgescu <carlosgeorgescu@fb.com>
2022-06-24 16:48:06 -0700

    added sliding text + images for intro screen

--- app/src/main/java/com/example/foundy/MainActivity.java
+++ app/src/main/java/com/example/foundy/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.foundy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SlideLayoutAdapter sliderAsapter;
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+    private TextView[] mDots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sliderAsapter = new SlideLayoutAdapter(this);
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
+
+        mSlideViewPager.setAdapter(sliderAsapter);
+
+        addDotsIndicator();
+
     }
+
+
 }
