ff597d9ebf37c218cb8ce4cded4b5ef9891eb165
Carlos Georgescu <carlosgeorgescu@fb.com>
2022-06-24 17:10:50 -0700

    the 3 circles to represent the different slides

--- app/src/main/java/com/example/foundy/MainActivity.java
+++ app/src/main/java/com/example/foundy/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Html;
 import android.widget.LinearLayout;
@@ -27,9 +29,47 @@ public class MainActivity extends AppCompatActivity {
 
         mSlideViewPager.setAdapter(sliderAsapter);
 
-        addDotsIndicator();
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
 
     }
 
+    public void addDotsIndicator(int position){
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
 
+        for(int i =0; i < mDots.length; i++)
+        {
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(70);
+           mDots[i].setTextColor(Color.GRAY);
+
+            mDotLayout.addView(mDots[i]);
+
+        }
+
+        if(mDots.length > 0)
+        {
+            mDots[position].setTextColor(Color.RED);
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
