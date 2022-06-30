d5ac26d7d4ff70a70fc21e7f8294a9a1ca1e00d0
Carlos Georgescu <carlosgeorgescu@fb.com>
2022-06-30 13:14:04 -0700

    added date picker

--- app/src/main/java/com/example/foundy/uploadActivities/UploadLost.java
+++ app/src/main/java/com/example/foundy/uploadActivities/UploadLost.java
@@ -2,15 +2,53 @@ package com.example.foundy.uploadActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.foundy.R;
 
+import java.util.Calendar;
+
 public class UploadLost extends AppCompatActivity {
+
+    DatePickerDialog.OnDateSetListener setListener;
+    EditText setDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_lost);
+
+        setDate = findViewById(R.id.selectDateText);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        setDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(UploadLost.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month++;
+                String date = dayOfMonth +" / " + month + " / " + year;
+                setDate.setText(date);
+            }
+        };
+
+
     }
 }
