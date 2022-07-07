7d4ff1370ffc6261e7b5111594114b5c761c0fa6
Carlos Georgescu <carlosgeorgescu@fb.com>
2022-07-06 18:04:37 -0700

    made marker on map appear on your current location if allowed by user via permissions + made the marker draggable

--- app/src/main/java/com/example/foundy/Fragments/MapsFragment.java
+++ app/src/main/java/com/example/foundy/Fragments/MapsFragment.java
@@ -1,23 +1,56 @@
 package com.example.foundy.Fragments;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.foundy.MapActivity;
 import com.example.foundy.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.concurrent.Executor;
 
 public class MapsFragment extends Fragment {
+
+    public GoogleMap map;
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location lastKnownLocation;
+
+    private boolean locationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int DEFAULT_ZOOM = 15;
+    private double currentLong;
+    private double currentLat;
+    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
+    // The entry point to the Fused Location Provider.
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -30,13 +63,30 @@ public class MapsFragment extends Fragment {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+        @RequiresApi(api = Build.VERSION_CODES.N)
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+            map = googleMap;
+
+            LatLng sydney = new LatLng(currentLat, currentLong);
+
+            googleMap.addMarker(new MarkerOptions()
+                    .position(sydney)
+                    .title("Marker in Syd")
+                    .draggable(true));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
         }
     };
+
+    public void setLatAndLong(double lat, double longitude){
+        currentLat = lat;
+        currentLong = longitude;
+    }
+
+
 
     @Nullable
     @Override
--- app/src/main/java/com/example/foundy/MapActivity.java
+++ app/src/main/java/com/example/foundy/MapActivity.java
@@ -1,27 +1,76 @@
 package com.example.foundy;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.foundy.Fragments.MapsFragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class MapActivity extends AppCompatActivity{
+    private FusedLocationProviderClient fusedLocationClient;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        addFragment();
+
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                            } else {
+                                // No location access granted.
+                            }
+                        }
+                );
+
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            addFragment(location.getLatitude(), location.getLongitude());
+                        }
+                    }
+                });
     }
 
-    private void addFragment(){
+    private void addFragment(double lat, double longi){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         MapsFragment mapsFragment = new MapsFragment();
+        mapsFragment.setLatAndLong(lat, longi);
         fragmentTransaction.add(R.id.googleMapView, mapsFragment);
         fragmentTransaction.commit();
     }
