21fbaad4169a9b0e0e679186cd6688c7a1eb7644
Carlos Georgescu <carlosgeorgescu@fb.com>
2022-07-15 05:55:27 -0700

    created recycler view and referenced in code

--- app/src/main/java/com/example/foundy/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/foundy/Fragments/HomeFragment.java
@@ -2,13 +2,17 @@ package com.example.foundy.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.foundy.R;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,10 +21,25 @@ import com.example.foundy.R;
  */
 public class HomeFragment extends Fragment {
 
+    private RecyclerView mRvPosts;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRvPosts = view.findViewById(R.id.rvPosts);
+    }
+
 }
