2aef03dff24b12ad14558456e361016b85990f36
Carlos Georgescu <carlosgeorgescu@fb.com>
2022-07-18 15:28:55 -0700

    added adapter for profile

---
+++ app/src/main/java/com/example/foundy/Adapters/ProfileMatchedAdapter.java
@@ -0,0 +1,33 @@
+package com.example.foundy.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.foundy.Fragments.FoundMatched;
+import com.example.foundy.Fragments.LostMatched;
+
+public class ProfileMatchedAdapter extends FragmentStateAdapter {
+
+    public ProfileMatchedAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position)
+    {
+        switch(position){
+            case 0:
+                return new FoundMatched();
+            case 1:
+                return new LostMatched();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
